175a16f7c554727c8e190b81a9e2b5e7951dd6b5
jakreewut <nongnack2504@gmail.com>
2026-02-03 11:01:51 +0700

    Add Process.java ver.1.1

--- src/Process.java
+++ src/Process.java
@@ -1,7 +1,36 @@
 public class Process {
-	public void input( int a ) {}
-	
-	public void input( String b ) {}
-	
-	public void operate() {}
+	private double totalResult = 0;
+	private double currentNum = 0;
+	private String currentOp = "+";
+
+	//Override
+	public void input(int num) {
+		this.currentNum = num;
+	}
+
+	//Override
+	public void input(String sing) {
+		this.currentOp = sing;
+	}
+
+	public void operate() {
+		switch(currentOp) {
+			case "+" : totalResult += currentNum; break;
+
+			case "-" : totalResult -= currentNum; break;
+
+			case "*" : totalResult *= currentNum; break;
+
+			case "/" : 
+			    if(currentNum != 0) totalResult /= currentNum;
+				else System.out.println("Error! : cannot divide by zero.");
+				break;
+		    case "%" : totalResult %= currentNum; break;
+		}
+		currentNum = 0;
+	}
+
+	public double getFinalResult() {
+		return totalResult;
+	}
 }
