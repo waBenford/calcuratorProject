17983a08da538fddc0c3c043a1e219f7e6b36e8d
waBenford <thanakorngithub@gmail.com>
2026-02-04 16:15:22 +0700

    Fix uiDisplay button [ 10 ] ---> [ 0 ]

--- src/Ui.java
+++ src/Ui.java
@@ -12,7 +12,7 @@ public class Ui {
 		System.out.println("=========================================");
 		System.out.println("     [ 1 ]  [ 2 ]  [ 3 ]  [ 4 ]  [ 5 ]   ");
 		System.out.println("-----------------------------------------");
-		System.out.println("     [ 6 ]  [ 7 ]  [ 8 ]  [ 9 ]  [ 10 ]  ");
+		System.out.println("     [ 6 ]  [ 7 ]  [ 8 ]  [ 9 ]  [ 0 ]   ");
 		System.out.println("-----------------------------------------");
 		System.out.println("     [ - ]  [ + ]  [ * ]  [ % ]  [ / ]   ");
 		System.out.println("-----------------------------------------");
